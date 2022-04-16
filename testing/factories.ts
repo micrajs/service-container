a19f8996477c60135084673c8f4f2324c5fee15d
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-04-16 17:55:47 -0500

    style: fixed lint warning

--- testing/factories.ts
+++ testing/factories.ts
@@ -1,6 +1,7 @@
 import fake from '@micra/faker/complete';
 import type {Binding} from '@/data/classes/Bindings';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const BindingFactory = fake.factory<Binding<any>>((fake) => {
   const to = fake.uuid();
   return {
